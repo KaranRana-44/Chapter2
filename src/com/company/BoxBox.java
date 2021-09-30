66128803ce5719ef9e57c5d489efafdeb94fb84e
karanrana-44 <karanrana@billericak12.com>
2021-09-30 12:04:28 -0400

    constructors, class etc.

--- src/com/company/BoxBox.java
+++ src/com/company/BoxBox.java
@@ -32,11 +32,25 @@ public class BoxBox {
     // accessor methods
 
     public void setHeight (double h){
-        height=h;
+        if (h > 0) {
+            height = h;
+        }
+
     }
 
     public double getHeight(){
         return height;
+    }
+
+    public double volume(){
+        return length*width*height;
+    }
+
+    public String toString(){
+        String s = "A box with dimensions " + height + " " + width + " " + length;
+        s+= " It's color is" + color + " and has a lid?" + hasALid;
+        return s;
+
     }
 
     public static void main(String[] args) {
@@ -47,6 +61,15 @@ public class BoxBox {
         System.out.println(b1.getHeight());
         b1.setHeight(9.9);
         System.out.println(b1.getHeight());
+        System.out.println(b1.volume());
+        System.out.println(b2.volume());
+        System.out.println(b3.volume());
+
+        int x=5;
+        String name="Betty Boop";
+        System.out.println(x);
+        System.out.println(name);
+        System.out.println(b1);
 
 
     }
